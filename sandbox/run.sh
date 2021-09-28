fe09880eb801435326c459a90a24ef7a5886e793
Ash <glaxyinfinite@outlook.com>
2021-09-28 23:01:23 +0800

    C language support

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -10,12 +10,9 @@
 #
 # $2:    The path to the source file in the container.
 
-bin=
-fullName=
-
 # Loop read symbolic link.
 #       $1:    Which bin
-loopReadLink(){
+getBin(){
     bin=`which $1`
     while [ -L "$1" ]; do
         bin=`readlink $bin`
@@ -23,18 +20,44 @@ loopReadLink(){
 }
 
 case $1 in
+    # java
+    1)
+        getBin "javac"
+        mv $2 "$2.java"
+        compiler=$bin
+        compilerArgs="$bin -d . $2.java"
+        runner="${bin%/*}/java"
+        runnerArgs="$runner -cp $CLASSPATH:${2%/*} -Djava.security.manager -Dfile.encoding=UTF-8 -Djava.security.policy==/etc/java_policy -Djava.awt.headless=true Main"
+        ;;
+
     # python2
     2)
-        loopReadLink "python2"
-        fullName=$2".py"
-        mv $2 $fullName
+        getBin "python2"
+        mv $2 "$2.py"
+        compiler=
+        compilerArgs=
+        runner=$bin
+        runnerArgs="$bin $2.py"
         ;;
 
     # python3
     3)
-        loopReadLink "python3"
-        fullName=$2".py"
-        mv $2 $fullName
+        getBin "python3"
+        mv $2 "$2.py"
+        compiler=
+        compilerArgs=
+        runner=$bin
+        runnerArgs="$bin $2.py"
+        ;;
+
+    # C
+    5)
+        getBin "gcc"
+        mv $2 "$2.c"
+        compiler=$bin
+        compilerArgs="$bin -O2 -w -std=c99 -lm -o $2 $2.c"
+        runner=$2
+        runnerArgs=
         ;;
 
     # ...
@@ -44,5 +67,26 @@ case $1 in
         ;;
 esac
 
-# Call the sandbox runner as the commit user.
-su commit -c "/commit/run $bin $fullName"
+# Is debug
+if [ ! -z "$3" ]; then
+    echo "compiler: $compiler"
+    echo "compilerArgs: $compilerArgs"
+    echo "runner: $runner"
+    echo "runnerArgs: $runnerArgs"
+fi
+
+# If it needs to compile
+if [ ! -z "$compiler" ]; then
+    compileResult=`/commit/run -b $compiler -p "$compilerArgs"`
+    if [ $? -ne 0 ]; then
+        echo "Compile error: $compileResult"
+        exit -1
+    fi
+fi
+
+# If it needs to run arguments
+if [ ! -z "$runnerArgs" ]; then
+    su commit -c "/commit/run -s -b $runner -p '$runnerArgs'"
+else
+    su commit -c "/commit/run -s -b $runner"
+fi
